701cef31c7a97f7d9ad4f9515104b2d15b7aa471
parakhchoudhary1208 <0808cs181119.ies@ipsacademy.org>
2024-01-29 12:43:29 +0530

    Redux ToolKit Todo Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import Todo from "./Components/Todo"
+import AddTodo from "./Components/AddTodo"
 
 function App() {
 
   return (
     <>
+        <AddTodo/>
+        <Todo/>
     </>
   )
 }
